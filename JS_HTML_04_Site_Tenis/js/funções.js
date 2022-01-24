41ba483513441b81edd26b5214e36646102f323f
EdinaldoCicero <68930411+EdinaldoCIcero@users.noreply.github.com>
2022-01-24 19:43:21 -0300

    Add files via upload

---
+++ JS_HTML_04_Site_Tenis/js/funções.js
@@ -0,0 +1,35 @@
+
+
+function image_buttons_blocks_azul( id_name ) {
+	// body...
+	document.getElementById(id_name).src='img/marcardor_azul.png'
+
+	if (id_name == "imgblockbutton_1") {
+	  document.getElementById("img_sapatos_gandes").src='img/tenis_grand_1.png' ;
+	}
+
+	else if (id_name == "imgblockbutton2") {
+	  document.getElementById("img_sapatos_gandes").src='img/tenis_grand_2.png' ;
+	}
+
+	else if (id_name == "imgblockbutton3") {
+	  document.getElementById("img_sapatos_gandes").src='img/tenis_grand_3.png' ;
+	}
+
+	//} else {
+	// document.getElementById("img_sapatos_gandes").src='img/tenis_grand_1.png' ;}//
+
+
+}
+	
+	
+function image_buttons_blocks_branco() {
+	// body...
+	document.getElementById(id_name).src='img/marcador_branco_buttons.png'
+}
+
+
+
+
+
+
